8dbb6a9aeeb3deae0183ebe6e13b812cd38cb202
kuixir <kuixir@uci.edu>
2019-12-30 16:24:27 -0800

    feat(dialog): a dialog modal using reactions

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const Discord = require('discord.js');
 const { prefix } = require('./prefix.json');
 const imagePick = require('./src/imgPick');
+const dialog = require('./components/dialog');
+
 require('dotenv').config();
+
 const token = process.env.token;
 const client = new Discord.Client();
 const mongoose = require('mongoose');
+
+const ME = '660763160810094592';
+
+const choiceMap = {
+  '🇦': 'A',
+  '🇧': 'B',
+  '🇨': 'C',
+  '🇩': 'D',
+  '🇪': 'E',
+  '🇫': 'F',
+};
 
 client.once('ready', () => {
   console.log('Ready!');
@@ -14,13 +28,29 @@ client.on('message', message => {
   console.log(message.content);
 });
 
+client.on('messageReactionAdd', (reaction, user) => {
+  reaction
+    .fetchUsers()
+    .then(obj => {
+      if (ME != obj.firstKey(1)[0]) {
+        reaction.message.edit(`you chose ${choiceMap[reaction.emoji.name]}`, {
+          code: true,
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+});
+
 client.on('message', message => {
   if (message.content.startsWith(prefix)) {
     if (message.content === prefix + 'ping') {
       message.channel.send('Pong!');
     } else if (message.content === prefix + 'help') {
       const { help } = require('./components/help_text.json');
-      message.channel.send(help);
+      message.channel.send(help, { code: true });
     } else if (message.content === prefix + 'git') {
       message.channel.send('https://github.com/kg233/discordBot');
     } else if (message.content === prefix + 'kg') {
@@ -37,6 +67,8 @@ client.on('message', message => {
           console.log(err);
           throw err;
         });
+    } else if (message.content === prefix + 'dialog') {
+      dialog(message);
     }
   }
 });
