4b7260776f34718da162767b22c4d9ed1d2c65c3
kuixir <kuixir@uci.edu>
2019-12-30 23:21:44 -0800

    barebone of todo created

---
+++ components/todo.js
@@ -0,0 +1,37 @@
+//each time command that triggers a todo is called, a todo object is created.
+//constructor:
+//todos is fetched from the database, an array of todo objects..?
+//message is the message that is made by the bot after todo function has been called by the user
+//used to editing the message, handling reaction inputs.
+
+class Todos {
+  constructor(message, todos) {
+    this.message = message;
+    this.todos = todos;
+    this.cursor = 0;
+    this.pagesize = 6;
+    this.left = 0;
+    this.right = 0;
+    if (this.todos.length >= 6) {
+      this.right = 5;
+    } else {
+      this.right = this.todos.length - 1;
+    }
+  }
+
+  makeHeader() {
+    return 'todo list\n----------------------------\n';
+  }
+
+  render() {
+    this.message.edit(`${this.makeHeader()}this is render`, {
+      code: true,
+    });
+  }
+
+  handle(emojiUNI) {
+    this.message.edit(`${emojiUNI}`, { code: true });
+  }
+}
+
+module.exports = Todos;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const { prefix } = require('./prefix.json');
 const imagePick = require('./src/imgPick');
 const dialog = require('./components/dialog');
+const Todos = require('./components/todo');
 
 require('dotenv').config();
 
@@ -20,6 +21,29 @@ const choiceMap = {
   '🇫': 'F',
 };
 
+//todo work in progress
+const testTodos = [
+  { id: 1, description: 'do dishes', finished: false },
+  { id: 1, description: "fuck vb's ass", finished: false },
+  { id: 1, description: "fuck vb's ass again", finished: false },
+  { id: 1, description: "fuck vb's ass again2", finished: false },
+  { id: 1, description: "fuck vb's ass again3", finished: false },
+  { id: 1, description: "fuck vb's ass 4", finished: false },
+  { id: 1, description: "fuck vb's ass 5", finished: false },
+  { id: 1, description: "fuck vb's ass 6", finished: false },
+  { id: 1, description: "fuck vb's ass 7", finished: false },
+  { id: 1, description: "fuck vb's ass 8", finished: false },
+  { id: 1, description: "fuck vb's ass 9", finished: false },
+  { id: 1, description: "fuck vb's ass 10", finished: false },
+  { id: 1, description: 'do homework', finished: false },
+  { id: 1, description: 'play neko para', finished: false },
+  { id: 1, description: 'hit airplane', finished: false },
+];
+
+const lookup = {}; //change this variable's name
+
+//todo work in progress
+
 client.once('ready', () => {
   console.log('Ready!');
 });
@@ -28,14 +52,20 @@ client.on('message', message => {
   console.log(message.content);
 });
 
-client.on('messageReactionAdd', (reaction, user) => {
-  reaction
+client.on('messageReactionAdd', (msgrcn, user) => {
+  msgrcn
     .fetchUsers()
     .then(obj => {
       if (ME != obj.firstKey(1)[0]) {
-        reaction.message.edit(`you chose ${choiceMap[reaction.emoji.name]}`, {
-          code: true,
-        });
+        //making sure it not the bot
+        // msgrcn.message.edit(`you chose ${choiceMap[msgrcn.emoji.name]}`, {
+        //   code: true,
+        // });
+        let msgid = msgrcn.message.id;
+        if (msgid in lookup) {
+          msgrcn.remove(user);
+          lookup[msgid].handle(msgrcn.emoji.name);
+        }
       }
     })
     .catch(err => {
@@ -69,6 +99,34 @@ client.on('message', message => {
         });
     } else if (message.content === prefix + 'dialog') {
       dialog(message);
+    } else if (message.content === prefix + 'todo') {
+      let id = message.author.id;
+      //fetch the todo list from mongoDB
+      message.channel
+        .send('loading', { code: true })
+        .then(msg => {
+          msg
+            .react('⬆️')
+            .then(rct => {
+              rct.message.react('⬇️').then(rct => {
+                rct.message.react('⬅️').then(rct => {
+                  rct.message.react('➡️');
+                });
+              });
+            })
+            .catch(err => {
+              console.log(err);
+              throw err;
+            });
+
+          let temp = new Todos(msg, testTodos);
+          lookup[msg.id] = temp;
+          temp.render();
+        })
+        .catch(err => {
+          console.log(err);
+          throw err;
+        });
     }
   }
 });
